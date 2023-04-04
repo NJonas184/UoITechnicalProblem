64d52d592c13d8e9c63bb7aed400deb074cf1881
MrDowns <mistervictordowns@gmail.com>
2023-04-04 16:15:00 -0500

    Added solution folder to run code in

--- solution/LetterService.cs
+++ solution/LetterService.cs
@@ -1,6 +1,17 @@
 using System;
 using System.IO;
 
+
+public interface ILetterService {
+
+    /// <summary>
+    /// Combine two letter files into one file.
+    /// <param name="inputFile1">File path for the first letter.</param>
+    /// <param name="inputFile2">File path for the second letter.</param>
+    /// <param name="resultFile">File path for the combined letter.</param>
+    void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
+}
+
 public class LetterService : ILetterService {
 
 
@@ -15,6 +26,11 @@ public class LetterService : ILetterService {
         string combinedFile = $"{fileOne}\n\n{fileTwo}";
 
         File.WriteAllText(resultFile, combinedFile);
+    }
+
+    public static void Main(string[] args){
+
+        System.Console.WriteLine($"Hello World!");
     }
 
 }
