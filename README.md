9b0a3012978347dda8325c8f53b40910537ae39c
MrDowns <mistervictordowns@gmail.com>
2023-04-05 11:42:10 -0500

    Updated ReadMe and cleaned code of checks.

---
+++ README.md
@@ -0,0 +1,7 @@
+# UoI FBIS Technical solution
+This repository is my solution to the technical problem given to me by the FBIS department at the University of Iowa.
+
+## Testing
+In order to test it, simply add dated folders into the Input Admission and Scholarship folders with admission and scholarship files respectively. In order to test this, simply navigate to the "solution" folder and execute the "LetterServices.cs" file using the .NET executor like such:
+
+`dotnet run`
--- solution/LetterService.cs
+++ solution/LetterService.cs
@@ -10,10 +10,10 @@ public interface ILetterService {
     /// <param name="inputFile2">File path for the second letter.</param>
     /// <param name="resultFile">File path for the combined letter.</param>
     void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile);
-}
+} //ILetterSErvice
 
 public class LetterService : ILetterService {
-
+    //Class for LetterService solution
     static string PARENT = "solution";
     static string PARENTDIRECTORY = $"{Directory.GetParent(PARENT)}";
     static string PROJECTDIRECTORY = $"{Directory.GetParent(PARENTDIRECTORY)}";
@@ -24,10 +24,7 @@ public class LetterService : ILetterService {
     static string OUTPUT_PATH = $"{PROJECTDIRECTORY}/CombinedLetters/Output";
 
     public void CombineTwoLetters(string inputFile1, string inputFile2, string resultFile) {
-        // 1. Read both files
-        // 2. take data from both files and combine
-        // 3. check for appropriate dated folder using inputfile paths
-        // 4. Write in resultFile
+        // Combines two letters and writes the result in resultFile
 
         string fileOne = File.ReadAllText(inputFile1);
         System.Console.WriteLine(fileOne);
@@ -37,39 +34,38 @@ public class LetterService : ILetterService {
         System.Console.WriteLine(combinedFile);
 
         File.WriteAllText(resultFile, combinedFile);
-    }
+    } //CombineTwoLetters
 
     public List<string> CheckScholarships(string admissionFolder, string[] filesArray) {
-        //returns input files that match
-        
+        //Checks to see if id is present in both relevant folders, then calls CombineTwoLetters function.
+        //Returns a list of ids
         
         var scholarshipFolder = $"{SCHOLARSHIP_PATH}/{admissionFolder}";
         List<string> idList = new List<string>();
         foreach(String file in filesArray){
-            System.Console.WriteLine(file);
+            
             string temp = file.Split('-')[1];
             string id = temp.Split('.')[0];
-            System.Console.WriteLine(id); //Delete check
+            
             string scholarshipFile = $"scholarship-{id}.txt";
-            System.Console.WriteLine($"{scholarshipFolder}/{scholarshipFile}");
+            
             if(File.Exists($"{scholarshipFolder}/{scholarshipFile}")){
                 System.Console.WriteLine("True");
                 string resultPath = $"{OUTPUT_PATH}/{admissionFolder}";
                 if(Directory.Exists(resultPath)!= true){
                     Directory.CreateDirectory(resultPath);
-                }
+                } //if
                 string resultFile = $"{resultPath}/CombinedLetters-{id}.txt";
                 string admissionFilePath = $"{file}";
                 string scholarshipFilePath = $"{scholarshipFolder}/{scholarshipFile}";
 
-                //Needs to be called by an object, change functions to be non-static and run using an object.
                 CombineTwoLetters(admissionFilePath, scholarshipFilePath, resultFile);
                 idList.Add(id);
-            }
-        }
+            } //if
+        } //foreach
         return idList;
         
-    }
+    } //CheckScholarship
 
 
 
@@ -77,7 +73,7 @@ public class LetterService : ILetterService {
         //Function that runs through all admission folders and checks corresponding scholarship folders
         var admissionDirectories = Directory.GetDirectories(ADMISSION_PATH);
         List<string> idList = new List<string>();
-        //Add solution string
+        //List of ids for later
         foreach(string folder in admissionDirectories){
             string[] folderArray = folder.Split(@"\");
             string folderName = folderArray[folderArray.Length - 1];
@@ -87,22 +83,22 @@ public class LetterService : ILetterService {
                 List<string> temp = CheckScholarships(folderName, Directory.GetFiles(folder));
                 foreach(string id in temp){
                     idList.Add(id);
-                }
+                } //foreach
                 
-            }
+            } //if
             Directory.Move(folder, $"{ARCHIVE_PATH}/Admission/{folderName}");
             var scholarshipFolder = $"{SCHOLARSHIP_PATH}/{folderName}";
             if(Directory.Exists(scholarshipFolder)){
                 //Check to see if scholarship exists
                 Directory.Move(scholarshipFolder,$"{ARCHIVE_PATH}/Scholarship/{folderName}");
-            }
+            } //if
             
-        }
+        } //foreach
         return idList;
-    }
+    } //CombineAllLetters
 
     public static void Main(string[] args) {
-        
+        //Main function
         LetterService letterService = new LetterService();
         List<string> idList = letterService.CombineAllLetters();
         DateTime today = DateTime.Today;
@@ -113,12 +109,12 @@ public class LetterService : ILetterService {
         foreach(string id in idList){
             //System.Console.WriteLine(id);
             report = report + $"\n\t{id}";
-        }
+        } //foreach
 
         System.Console.WriteLine(report);
 
         File.WriteAllText($"{OUTPUT_PATH}/{today.ToString("MM-dd-yyyy")}-Report.txt", report);
     
-    }
+    } //Main
 
-}
+} //LetterService
